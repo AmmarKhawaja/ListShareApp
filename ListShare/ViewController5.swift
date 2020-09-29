99a9632be2d4c26092c81389a10ddff281d05292
AmmarKhawaja <ammarkhawaja01@gmail.com>
2020-09-28 20:36:10 -0700

    done

--- ListShare/ViewController5.swift
+++ ListShare/ViewController5.swift
@@ -1,14 +1,21 @@
 //ITEM INFO SCREEN
+import UIKit
 import Foundation
 import FirebaseDatabase
+
 var ref4 : DatabaseReference?
+
 class ViewController5: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ref4 = Database.database().reference()
+        
         print(getlistKey)
         print(transfergetList)
-        ref4?.child("Lists").child(getlistKey).child("addedItems").child(transfergetList).observeSingleEvent(of: .value, with: { (snapshot) in
+        ref4?.child("Lists").child(getlistKey).child("addedItems").child(transfergetList).child("date").setValue("sep11")
+        ref4?.child("Lists").child(getlistKey).child("addedItems").child(transfergetList).observe(.value, with: { (snapshot) in
             print("byUser")
             print(snapshot.value!)
         })
